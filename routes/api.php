63df90a0cd503b22a613d6be2d8d60085a0544d6
AbyFwz <abyanfawwaz77@gmail.com>
2022-06-05 11:34:32 +0700

    Make dummy url in API Route

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,32 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
+Route::get('/request-read', function () {
+    $data = [
+        [
+            'nama' => 'Arsip satu',
+            'nomor' => 'FRM-BAA-01',
+            'deskripsi' => 'Ini arsip'
+        ],
+        [
+            'nama' => 'Arsip dua',
+            'nomor' => 'FRM-BAA-02',
+            'deskripsi' => 'Ini arsip'
+        ]
+    ];
+    return response()->json(['message' => 'Request sukses', 'data' => $data]);
+});
+
+Route::post('/request-post', function (Request $request) {
+    $data = [
+        'nama' => $request->nama,
+        'nomor' => $request->nomor,
+        'deskripsi' => $request->deskripsi
+    ];
+
+    return response()->json(['data' => $data]);
+});
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function () {
         return auth()->user();
