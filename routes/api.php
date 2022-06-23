5f9226a30c42c0cb5015c5e6ac96133fede39e31
AbyFwz <abyanfawwaz77@gmail.com>
2022-06-23 23:12:49 +0700

    Make new mene (Kategori)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\Arsip\ArsipController;
 use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\KategoriController;
 use App\Http\Controllers\API\Siswa\SiswaController;
 use App\Http\Controllers\API\Surat\SuratKeluarController;
 use App\Http\Controllers\API\Surat\SuratMasukController;
@@ -28,52 +29,15 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-
-Route::get('/request-read', function () {
-    $data = [
-        [
-            'nama' => 'Arsip satu',
-            'nomor' => 'FRM-BAA-01',
-            'deskripsi' => 'Ini arsip'
-        ],
-        [
-            'nama' => 'Arsip dua',
-            'nomor' => 'FRM-BAA-02',
-            'deskripsi' => 'Ini arsip'
-        ]
-    ];
-    return response()->json(['message' => 'Request sukses', 'listarsip' => $data]);
-});
-
-Route::post('/request-post', function (Request $request) {
-    $data = [
-        'nama' => $request->nama,
-        'nomor' => $request->nomor,
-        'deskripsi' => $request->deskripsi
-    ];
-
-    return response()->json(['data' => $data]);
+Route::get('/login', function ()
+{
+    return abort(404);
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/profile', function () {
-        // $user = json_encode(auth()->user());
-        $user = [
-            [
-                'nama' => 'Abyan Fawwaz',
-                'email' => 'aby@aby.com'
-            ],
-            [
-                'nama' => 'Erni Srihartini',
-                'email' => 'erni@erni.com'
-            ]
-        ];
-        $user = json_encode($user);
-        return response()->json(['message' => 'Ini Profile', 'user' => $user]);
-    });
-
     Route::apiResource('siswa', SiswaController::class,);
     Route::apiResource('arsip', ArsipController::class);
+    Route::apiResource('kategori', KategoriController::class);
     Route::apiResource('surat-masuk', SuratMasukController::class);
     Route::apiResource('surat-keluar', SuratKeluarController::class);
     Route::apiResource('tamu-dinas', TamuDinasController::class);
