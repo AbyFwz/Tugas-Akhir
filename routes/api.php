23ec2c88af536738f8443df01427a16ad0a22968
AbyFwz <abyanfawwaz77@gmail.com>
2022-06-28 21:21:13 +0700

    Update kategori route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\API\Arsip\ArsipController;
 use App\Http\Controllers\API\Auth\AuthController;
-use App\Http\Controllers\API\KategoriController;
+use App\Http\Controllers\API\Arsip\KategoriController;
 use App\Http\Controllers\API\Siswa\SiswaController;
 use App\Http\Controllers\API\Surat\SuratKeluarController;
 use App\Http\Controllers\API\Surat\SuratMasukController;
@@ -29,8 +29,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/login', function ()
-{
+Route::get('/login', function () {
     return abort(404);
 });
 
