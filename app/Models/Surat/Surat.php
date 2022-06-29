078a7df38f3317d4a6c7ffa71c5553bff6982e5d
AbyFwz <abyanfawwaz77@gmail.com>
2022-06-29 13:07:11 +0700

    Update relation models

--- app/Models/Surat/Surat.php
+++ app/Models/Surat/Surat.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Surat;
 
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
